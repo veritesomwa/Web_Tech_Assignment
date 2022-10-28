631cbb0c96e50f0fa445bca49e1cec8802213b16
Verite Lutula <veritelutula@gmail.com>
2022-10-28 21:26:04 +0200

    Finish Question 3 JS form Validation: Need to do now Cookies

---
+++ Question3/script.js
@@ -0,0 +1,61 @@
+let btnSubmit = document.getElementById("btnSubmit");
+let btnReset = document.getElementById("btnReset");
+let firstname = document.getElementById("firstname");
+let lastname = document.getElementById("lastname");
+let address = document.getElementById("address");
+let email = document.getElementById("email");
+let radio_male = document.getElementById("radio_male");
+let radio_female = document.getElementById("radio_female");
+let number = document.getElementById("number");
+let dropd = document.getElementById("dropD");
+
+
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return (true);
+  }else{
+    return (false);
+  }
+    
+}
+
+
+
+function isEmpty(text) {
+    const REGEXP = /^$/;
+    return REGEXP.test(text);
+}
+
+btnSubmit.addEventListener("click", (e)=> {
+    e.preventDefault();
+    try {
+        if (isEmpty(firstname.value)) throw `Firstname is required`; 
+        if (isEmpty(lastname.value)) throw `Lastname is required`;
+        if (isEmpty(address.value)) throw `Address is required`;
+
+        if (!(radio_female.checked == false & radio_male.checked == false) == 0) throw `Gender is required. Select One`;
+
+        if (isEmpty(email.value)) throw `Email is required`;
+        if (!ValidateEmail(email.value)) throw `Invalid Email format`;
+
+        if (isEmpty(number.value)) throw `Mobile Number is required`;
+        if (isEmpty(dropd.value)) throw `Location is required`;
+    } catch (error) {
+        alert(error);
+    }
+});
+
+btnReset.addEventListener("click", e => {
+    e.preventDefault()
+    firstname.value = "";
+    lastname.value = "";
+    address.value = "";
+    email.value = "";
+    number.value = "";
+    number.value = "";
+    dropd.value = "";
+    radio_female.checked = false;
+    radio_male.checked = false;
+
+});
